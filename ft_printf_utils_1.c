1024ed2dd58b3967122865eb3e84a877fb2632bf
brkncookie <the0007end@gmail.com>
2022-06-11 00:05:09 +0100

    ft_str almost there

--- ft_printf_utils_1.c
+++ ft_printf_utils_1.c
@@ -1,6 +1,39 @@
 #include "include/ft_printf.h"
-char	*ft_str(char *fptr, int	flen, va_list *ap)
+char	*ft_str(char *fptr, long int flen, va_list *ap)
 {
-	return(ft_strdup(va_arg(*ap, char *)));
+	char	*ns;
+	char	*os;
+	int	msze;
+	long	int	len;
+	/* precision, width, - flag */
+	os = ft_strdup(va_arg(*ap, char *));
+	ns = os;
+	len = 1;
+	if(flen > 2)
+		while(len < flen)
+		{
+			if(fptr[len] == '.')
+			{
+				msze = atoi(&(fptr[len+1]));
+				if(msze == 0)
+				{
+					ns = malloc(sizeof(*ns));
+					*ns = 0;
+				}
+				/* else if (msze < 0) */
+				/* 	ns = os; */
+				else if (msze < ft_strlen(os))
+				{
+					ns = malloc((msze + 1) * sizeof(*ns));
+					ft_memcpy(ns, os, msze);
+					ns[msze] = 0;
+					free(os);
+				}
+
+			}
+			len++;
+		}
+	return(ns);
+
 }
 
